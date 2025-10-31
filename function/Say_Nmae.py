9887d73197237ace372ca53503509ec070cb8dd4
imrane-max <imrane2015su@gmail.com>
2025-10-31 17:46:23 +0100

    Create a Say name Function in Functions Folder

---
+++ function/Say_Nmae.py
@@ -0,0 +1,4 @@
+def Say_name(name):
+    print(f"Hello, {name}")
+
+Say_name(input("Enter your name: "))
--- function/main.py
+++
@@ -1,5 +0,0 @@
-def say_name(name):
-    print(f"Hello, {name}!")
-
-
-say_name(input("Enter your name: "))
